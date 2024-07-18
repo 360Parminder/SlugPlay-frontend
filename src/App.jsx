3cbf0ef45dba90051d75f09f194daf062623f766
360Parminder <yesask8@gmail.com>
2024-07-19 00:06:56 +0530

    added License

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import HomePage from './Pages/HomePage'
-
+import '@ionic/react/css/core.css';
+import { setupIonicReact } from '@ionic/react';
+setupIonicReact();
  const App=()=> {
 
   return (
--- src/Components/Navbard.jsx
+++ src/Components/Navbard.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import { IonIcon } from '@ionic/react';
 const Navbard = () => {
   return (
     <div className=' h-16 w-screen bg-black flex row text-slate-200 justify-between px-10 items-center'>
         <div><p>logo</p></div>
-        <div><p>search</p></div>
+        <div className=' w-1/3 flex py-1 px-3 bg-slate-600 rounded-3xl justify-between '>
+           <button>Movie</button>
+           <button>Series</button>
+           <button>Originals</button>
+           <button>
+            <IonIcon name='cafe-outline' color='#fff' size='20'/>
+           </button>
+            
+            </div>
         <div><p>profile</p></div>
     </div>
   )
